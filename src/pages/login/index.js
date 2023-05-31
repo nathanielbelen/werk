c2df12c0c582b7cd9dc38972cb88b3d7bf60b4b2
nathanielbelen <nathaniel.belen@gmail.com>
2023-05-31 12:18:27 -0700

    changed login to redirect to homepage is user is already logged in

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,49 +1,31 @@
 import { Auth } from '@supabase/auth-ui-react'
 import { useUser, useSupabaseClient } from '@supabase/auth-helpers-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { useEffect, useState } from 'react'
 import { Box, Center } from '@chakra-ui/react'
+import { useRouter } from 'next/router';
 
 const LoginPage = () => {
   const supabaseClient = useSupabaseClient()
   const user = useUser()
-  const [data, setData] = useState()
+  const router = useRouter();
 
-  useEffect(() => {
-    async function loadData() {
-      const { data } = await supabaseClient.from('applications').select('*')
-      setData(data)
-    }
-    // only run query once user is logged in.
-    if (user) loadData()
-  }, [user])
-
-  if (!user)
-    return (
-      <Box>
-        <Center h='800px'>
-          <Box w='500px'>
-            <Auth
-              redirectTo="http://localhost:3000/"
-              supabaseClient={supabaseClient}
-              appearance={{ theme: ThemeSupa }}
-              socialLayout="horizontal"
-            />
-          </Box>
-        </Center>
-
-      </Box>
-
-    )
+  if (user) {
+    router.push('/');
+  }
 
   return (
-    <>
-      <button onClick={() => supabaseClient.auth.signOut()}>Sign out</button>
-      <p>user:</p>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-      <p>client-side data fetching with RLS</p>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </>
+    <Box>
+      <Center h='800px'>
+        <Box w='500px'>
+          <Auth
+            redirectTo="http://localhost:3000/"
+            supabaseClient={supabaseClient}
+            appearance={{ theme: ThemeSupa }}
+            socialLayout="horizontal"
+          />
+        </Box>
+      </Center>
+    </Box>
   )
 }
 
