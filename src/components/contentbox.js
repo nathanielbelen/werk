cd7402f1f724ad1f792c9cfb0892011ff6b42bf9
nathanielbelen <nathaniel.belen@gmail.com>
2023-06-08 10:19:14 -0700

    added contentbox component, apps load changes on accordion open, changed bg to match cb

---
+++ src/components/contentbox.js
@@ -0,0 +1,13 @@
+import { Box, Heading, useColorModeValue } from '@chakra-ui/react'
+
+export default function ContentBox({ children, heading }) {
+  return (
+    <Box
+      bg={useColorModeValue('white', 'gray.800')}
+      color={useColorModeValue('gray.800', 'white')}
+      rounded={'lg'} p={'5'} m={'2'}>
+      <Heading color={useColorModeValue('gray.500', 'gray.300')} size='1xl' pb='2'>{heading.toUpperCase()}</Heading>
+      <Box>{children}</Box>
+    </Box>
+  )
+}
