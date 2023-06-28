5337d14e21b4bafbed47f14256683eeb9ad6cee2
nathanielbelen <nathaniel.belen@gmail.com>
2023-06-27 20:10:29 -0700

    created addapplicationform, refactored to use reusable components

--- src/pages/list/components/AddApplicationForm.js
+++ src/pages/list/components/AddApplicationForm.js
@@ -10,10 +10,9 @@ import {
   Textarea,
   useColorModeValue
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function AddApplicationForm({ handleStop }) {
-
   const [formData, setFormData] = useState({
     company: "",
     position: "",
@@ -27,7 +26,6 @@ export default function AddApplicationForm({ handleStop }) {
     status: "",
     category: ""
   });
-
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     setFormData((prevState) => ({
@@ -35,115 +33,95 @@ export default function AddApplicationForm({ handleStop }) {
       [name]: type === "checkbox" ? checked : value
     }));
   };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(formData);
   };
-
+  const commonProps = {
+    formData,
+    onChange: handleChange
+  };
   return (
     <form onSubmit={handleSubmit}>
-      <FormControl>
-        <FormLabel>Company</FormLabel>
-        <Input name="company" value={formData.company} onChange={handleChange} />
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>Position</FormLabel>
-        <Input name="position" value={formData.position} onChange={handleChange} />
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>URL</FormLabel>
-        <Input name="url" value={formData.url} onChange={handleChange} />
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>Subtitle</FormLabel>
-        <Input name="subtitle" value={formData.subtitle} onChange={handleChange} />
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>Notes</FormLabel>
-        <Textarea name="notes" value={formData.notes} onChange={handleChange} />
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>Cover Letter</FormLabel>
-        <Checkbox name="cover_letter" checked={formData.cover_letter} onChange={handleChange}>
-          Include cover letter
-        </Checkbox>
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>Resume Number</FormLabel>
-        <Select name="resume_number" value={formData.resume_number} onChange={handleChange}>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </Select>
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>Location</FormLabel>
-        <Input name="location" value={formData.location} onChange={handleChange} />
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>Stage</FormLabel>
-        <Select name="stage" value={formData.stage} onChange={handleChange}>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </Select>
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>Status</FormLabel>
-        <Select name="status" value={formData.status} onChange={handleChange}>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </Select>
-      </FormControl>
-
-      <FormControl>
-        <FormLabel>Category</FormLabel>
-        <Input name="category" value={formData.category} onChange={handleChange} />
-      </FormControl>
-
+      <ControlledInput label='Company' name='company' {...commonProps} />
+      <ControlledInput label='Position' name='position' {...commonProps} />
+      <ControlledInput label='URL' name='url' {...commonProps} />
+      <ControlledInput label='Subtitle' name='subtitle' {...commonProps} />
+      <ControlledTextarea label='Notes' name='notes' {...commonProps} />
+      <ControlledCheckbox
+        label='Cover Letter'
+        name='cover_letter'
+        text='Include cover letter'
+        {...commonProps} />
+      <ControlledSelect
+        label='Resume Number'
+        name='resume_number'
+        options={[1, 2, 3, 4, 5]}
+        {...commonProps} />
+      <ControlledInput label='Location' name='location' {...commonProps} />
+      <ControlledSelect
+        label='Stage'
+        name='stage'
+        options={[1, 2, 3, 4, 5]}
+        {...commonProps} />
+      <ControlledSelect
+        label='Status'
+        name='status'
+        options={[1, 2, 3, 4, 5]}
+        {...commonProps} />
+      <ControlledInput label='Category' name='category' {...commonProps} />
       <Flex justifyContent={'center'} gap={'2'}>
-        <Button variant='solid'>Submit</Button>
+        <Button type="submit" variant='solid'>Submit</Button>
         <Button variant='outline' onClick={handleStop}>Cancel</Button>
       </Flex>
     </form>
   )
 }
 
-const ControlledSelect = ({ label, name, onChange, options }) => {
+const LabelledFormControl = ({ label, children }) => {
   return (
     <FormControl>
       <FormLabel>{label}</FormLabel>
-      <Select name={name} value={formData[name]} onChange={onChange}>
-        {options.map((option, idx) => {
-          <option value={option}>option</option>
-        })}
-      </Select>
+      {children}
     </FormControl>
   )
 }
 
-const ControlledInput = ({ label, name, onChange }) => {
+const ControlledSelect = ({ label, name, options, formData, onChange }) => {
   return (
-    <FormControl>
-      <FormLabel>{label}</FormLabel>
-      <Input name={name} value={formData[name]} onChange={onChange} />
-    </FormControl>
+    <LabelledFormControl label={label}>
+      <Select name={name} value={formData[name]} onChange={onChange}>
+        {options.map((option, idx) =>
+          <option value={option} key={`${name}_option_${idx}`}>{option}</option>
+        )}
+      </Select>
+    </LabelledFormControl>
   )
 }
+
+const ControlledInput = ({ label, name, onChange, formData }) => {
+  return (
+    <LabelledFormControl label={label}>
+      <Input name={name} value={formData[name]} onChange={onChange} />
+    </LabelledFormControl>
+  )
+}
+
+const ControlledCheckbox = ({ label, name, text, onChange, formData }) => {
+  return (
+    <LabelledFormControl label={label}>
+      <Checkbox name={name} checked={formData[name]} onChange={onChange}>
+        {text}
+      </Checkbox>
+    </LabelledFormControl>
+  )
+}
+
+const ControlledTextarea = ({ label, name, onChange, formData }) => {
+  return (
+    <LabelledFormControl label={label}>
+      <Textarea name={name} value={formData[name]} onChange={onChange} />
+    </LabelledFormControl>
+  )
+}
+
